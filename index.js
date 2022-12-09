85fd8a3bb455e372f4aba3a0f6f7265d52a02beb
Ankit <kumarankit14102002@gmail.com>
2022-12-09 15:40:43 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,17 @@
+// let body = document.getElementById('body')
+// let btn = document.getElementById('btn')
+// // let text = document.getElementById('text')
+
+// btn.addEventListener('click', toglebtn)
+// function toglebtn(){
+//     if(btn.textContent.includes('Bright')){
+//         body.style.backgroundColor = "black"
+//         // text.style.color = "white"
+//         btn.textContent = "Light Mode"
+//     }else{
+//         body.style.backgroundColor = "white"
+//         // text.style.color = "black"
+//         btn.textContent = "Bright"
+//     }
+// }
+
